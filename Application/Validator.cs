a1d9d447924f9f7a4fc55b7cf3684596c510af64
EliiseS <selingeliise@gmail.com>
2017-04-26 20:36:45 +0200

    Implemented  validation for various datatypes

--- Application/Validator.cs
+++ Application/Validator.cs
@@ -7,23 +7,46 @@ namespace EvilCorp.SlackStorage.WebBusinessApi.Business
 {
     public class Validator : IValidator
     {
-        private readonly IExceptionHandler _exceptionHandler;
+        private static readonly int DataStoreIdMaxLength = 105;
+        private static readonly int UserIdMaxLength = 12;
+        private static readonly int DataStoreNameMaxLength = DataStoreIdMaxLength - UserIdMaxLength - 1;
 
-        private LogLevel validatorLogLevel = LogLevel.Warning;
-
-        public LogLevel ValidatorLogLevel { get => validatorLogLevel; }
-
-        public Validator(IExceptionHandler exceptionHandler)
-        {
-            _exceptionHandler = exceptionHandler;
-        }
+        public LogLevel ValidatorLogLevel { get; } = LogLevel.Warning;
 
         public bool IsValidUserId(string id)
         {
-            if (id.Length > 12)
-                throw new ArgumentException("Length cannot be more than 12.", nameof(id));
+
+            return true;
+        }
+
+        public bool IsValidDataStoreId(string id)
+        {
+            if (id.Length > DataStoreIdMaxLength)
+                throw new ArgumentException(string.Format("Length cannot be more than {0}.", DataStoreIdMaxLength), nameof(id));
             if (string.IsNullOrEmpty(id))
                 throw new ArgumentException("The ID cannot be null or empty.", nameof(id));
+
+            return true;
+        }
+
+        public bool IsValidElementId(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+                throw new ArgumentException("The ID cannot be null or empty.", nameof(id));
+            if (!Guid.TryParse(id, out Guid newGuid))
+                throw new ArgumentException("Invalid guid.", nameof(id));
+
+            return true;
+        }
+
+        public bool IsValidDataStoreName(string dataStoreName)
+        {
+            if (dataStoreName.Length > DataStoreNameMaxLength)
+                throw new ArgumentException(string.Format("Length cannot be more than {0}.", DataStoreNameMaxLength), nameof(dataStoreName));
+            if (string.IsNullOrEmpty(dataStoreName))
+                throw new ArgumentException("The name cannot be null or empty.", nameof(dataStoreName));
+            if (JsonConvert.DeserializeObject(dataStoreName) == null)
+                throw new ArgumentException("Invalid JSON", nameof(dataStoreName));
 
             return true;
         }
@@ -31,11 +54,12 @@ namespace EvilCorp.SlackStorage.WebBusinessApi.Business
         public bool IsValidJson(string json)
         {
             if (string.IsNullOrEmpty(json))
-                return false;
+                throw new ArgumentException("Cannot be empty", nameof(json));
             if (json.Length > 2000 || json.Length < 2)
-                return false;
-            var result = _exceptionHandler.Run(() => JsonConvert.DeserializeObject(json));
-            return result != null;
+                throw new ArgumentException("Length cannot be more than 2000 or less than 2.", nameof(json));
+            if (JsonConvert.DeserializeObject(json) == null)
+                throw new ArgumentException("Invalid JSON", nameof(json));
+            return true;
         }
     }
 }
