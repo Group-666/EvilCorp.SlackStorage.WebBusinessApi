0700e4c20705abb66e0570ae0939f1944b907020
EliiseS <selingeliise@gmail.com>
2017-04-26 23:46:54 +0200

    Implemented unit test for ClientDataManager

--- EvilCorp.SlackStorage.WebBusinessApi.Application.Test/ClientDataManagerTests.cs
+++ EvilCorp.SlackStorage.WebBusinessApi.Application.Test/ClientDataManagerTests.cs
@@ -1,20 +1,16 @@
-using EvilCorp.SlackStorage.WebBusinessApi.Business;
 using EvilCorp.SlackStorage.WebBusinessApi.CrossCutting.Testing;
 using EvilCorp.SlackStorage.WebBusinessApi.Domain.Contracts;
-using EvilCorp.SlackStorage.WebBusinessApi.Domain.Entities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
 using System.Threading.Tasks;
 
-namespace EvilCorp.SlackStorage.WebBusinessApi.Data.Test
+namespace EvilCorp.SlackStorage.WebBusinessApi.Business.Test
 {
     [TestClass]
     public class ClientDataManagerTests : TestsFor<ClientDataManager>
     {
-        private readonly string _userId = "UserID";
-        private readonly string _dataStoreId = "DataStoreID";
-        private readonly string _result = "value: value1";
+        private readonly string _stringValue = "SomeString";
         private ExceptionHandler _exceptionHandler;
 
         protected override void OverrideMocks()
@@ -27,55 +23,360 @@ namespace EvilCorp.SlackStorage.WebBusinessApi.Data.Test
 
         #region ClientDataManage Tests
 
+        #region Create Tests
+
         [TestMethod]
-        public void GetAll_IdIsOK_RepositoryIsCalled()
+        public async Task Create_IdsAreValid_RepositoryIsCalled()
         {
             // Arrange
-            GetMockFor<IValidator>().Setup(v => v.IsValidUserId(_userId)).Returns(true);
+            SetupValidatorToBeValid();
 
             // Act
-            var result = Instance.GetAll(_userId);
+            await Instance.Create(_stringValue, _stringValue);
 
             // Assert
-            GetMockFor<IClientDataRespository>().Verify(r => r.GetAll(_userId), Times.Once());
+            GetMockFor<IClientDataRespository>().Verify(r => r.Create(_stringValue, _stringValue), Times.Once());
         }
 
         [TestMethod]
-        public void GetAll_IdIsInvalid_RepositoryIsNeverCalled()
+        public async Task Create_IdsAreInvalid_RepositoryIsNeverCalled()
         {
+            // Arrange
+            SetupValidatorToThrowExpection();
             // Act
-            var result = Instance.GetAll(_userId);
+            try
+            {
+                await Instance.Create(_stringValue, _stringValue);
+            }
+            catch
+            {
+                // Assert
+                GetMockFor<IClientDataRespository>().Verify(r => r.Create(_stringValue, _stringValue), Times.Never());
+            }
+        }
+
+        #endregion Create Tests
+
+        #region Post Tests
+
+        [TestMethod]
+        public async Task Post_IdsAreValid_RepositoryIsCalled()
+        {
+            // Arrange
+            SetupValidatorToBeValid();
+
+            // Act
+            await Instance.Post(_stringValue, _stringValue, _stringValue);
 
             // Assert
-            GetMockFor<IClientDataRespository>().Verify(r => r.GetAll(_userId), Times.Never());
+            GetMockFor<IClientDataRespository>().Verify(r => r.Post(_stringValue, _stringValue, _stringValue), Times.Once());
         }
 
         [TestMethod]
-        public void GetOne_IdsAreOK_RepositoryIsCalledK()
+        public async Task Post_IdsAreInvalid_RepositoryIsNeverCalled()
         {
             // Arrange
-            GetMockFor<IValidator>().Setup(v => v.IsValidUserId(_userId)).Returns(true);
+            SetupValidatorToThrowExpection();
+            // Act
+            try
+            {
+                await Instance.Post(_stringValue, _stringValue, _stringValue);
+            }
+            catch
+            {
+                // Assert
+                GetMockFor<IClientDataRespository>().Verify(r => r.Post(_stringValue, _stringValue, _stringValue), Times.Never());
+            }
+        }
+
+        #endregion Post Tests
+
+        #region GetAll Tests
+
+        [TestMethod]
+        public async Task GetAll_IdIsInvalid_RepositoryIsCalled()
+        {
+            // Arrange
+            SetupValidatorToBeValid();
 
             // Act
-            var result = Instance.GetAll(_userId);
+            await Instance.GetAll(_stringValue);
 
             // Assert
-            GetMockFor<IClientDataRespository>().Verify(r => r.GetAll(_userId), Times.Once());
+            GetMockFor<IClientDataRespository>().Verify(r => r.GetAll(_stringValue), Times.Once());
         }
 
         [TestMethod]
-        public async Task GetOne_IdsAreOK_ClientDataRepositoryAndLoggerThrowsAnException()
+        public async Task GetAll_IdIsInvalid_RepositoryIsNeverCalled()
         {
             // Arrange
-            GetMockFor<IClientDataRespository>().Setup(mock => mock.GetOne(_userId, _dataStoreId)).Throws(new Exception("Mooo"));
-            GetMockFor<ILogger>().Setup(mock => mock.Log(It.IsAny<string>(), It.IsAny<LogLevel>())).Throws(new Exception());
-            GetMockFor<IValidator>().Setup(v => v.IsValidUserId(It.IsAny<string>())).Returns(true);
+            SetupValidatorToThrowExpection();
 
             // Act
-            var result = await Instance.GetOne(_userId, _dataStoreId);
+            try
+            {
+                await Instance.GetAll(_stringValue);
+            }
+            catch
+            {
+                // Assert
+                GetMockFor<IClientDataRespository>().Verify(r => r.GetAll(_stringValue), Times.Never());
+            }
+        }
+
+        #endregion GetAll Tests
+
+        #region GetOne Tests
+
+        [TestMethod]
+        public async Task GetOne_IdsAreValid_RepositoryIsCalledK()
+        {
+            // Arrange
+            SetupValidatorToBeValid();
+
+            // Act
+            await Instance.GetOne(_stringValue, _stringValue);
 
             // Assert
-            Assert.IsNull(result);
+            GetMockFor<IClientDataRespository>().Verify(r => r.GetOne(_stringValue, _stringValue), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetOne_IdsAreInvalid_RepositoryIsNeverCalled()
+        {
+            // Arrange
+            SetupValidatorToThrowExpection();
+
+            // Act
+            try
+            {
+                await Instance.GetOne(_stringValue, _stringValue);
+            }
+            catch
+            {
+                // Assert
+                GetMockFor<IClientDataRespository>().Verify(r => r.GetOne(_stringValue, _stringValue), Times.Never());
+            }
+        }
+
+        #endregion GetOne Tests
+
+        #region GetElementAll Tests
+
+        [TestMethod]
+        public async Task GetElementAll_IdsAreValid_RepositoryIsCalled()
+        {
+            // Arrange
+            SetupValidatorToBeValid();
+
+            // Act
+            await Instance.GetElementAll(_stringValue, _stringValue);
+
+            // Assert
+            GetMockFor<IClientDataRespository>().Verify(r => r.GetElementAll(_stringValue, _stringValue), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetElementAll_IdsAreInvalid_RepositoryIsNeverCalled()
+        {
+            // Arrange
+            SetupValidatorToThrowExpection();
+            // Act
+            try
+            {
+                await Instance.GetElementAll(_stringValue, _stringValue);
+            }
+            catch
+            {
+                // Assert
+                GetMockFor<IClientDataRespository>().Verify(r => r.GetElementAll(_stringValue, _stringValue), Times.Never());
+            }
+        }
+
+        #endregion GetElementAll Tests
+
+        #region GetElementOne Tests
+
+        [TestMethod]
+        public async Task GetElementOne_IdsAreValid_RepositoryIsCalled()
+        {
+            // Arrange
+            SetupValidatorToBeValid();
+
+            // Act
+            await Instance.GetElementOne(_stringValue, _stringValue, _stringValue);
+
+            // Assert
+            GetMockFor<IClientDataRespository>().Verify(r => r.GetElementOne(_stringValue, _stringValue, _stringValue), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetElementOne_IdsAreInvalid_RepositoryIsNeverCalled()
+        {
+            // Arrange
+            SetupValidatorToThrowExpection();
+            // Act
+            try
+            {
+                await Instance.GetElementOne(_stringValue, _stringValue, _stringValue);
+            }
+            catch
+            {
+                // Assert
+                GetMockFor<IClientDataRespository>().Verify(r => r.GetElementOne(_stringValue, _stringValue, _stringValue), Times.Never());
+            }
+        }
+
+        #endregion GetElementOne Tests
+
+        #region DeleteAll Tests
+
+        [TestMethod]
+        public async Task DeleteAll_IdsAreValid_RepositoryIsCalled()
+        {
+            // Arrange
+            SetupValidatorToBeValid();
+
+            // Act
+            await Instance.DeleteAll(_stringValue);
+
+            // Assert
+            GetMockFor<IClientDataRespository>().Verify(r => r.DeleteAll(_stringValue), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task DeleteAll_IdsAreInvalid_RepositoryIsNeverCalled()
+        {
+            // Arrange
+            SetupValidatorToThrowExpection();
+            // Act
+            try
+            {
+                await Instance.DeleteAll(_stringValue);
+            }
+            catch
+            {
+                // Assert
+                GetMockFor<IClientDataRespository>().Verify(r => r.DeleteAll(_stringValue), Times.Never());
+            }
+        }
+
+        #endregion DeleteAll Tests
+
+        #region DeleteOne Tests
+
+        [TestMethod]
+        public async Task DeleteOne_IdsAreValid_RepositoryIsCalled()
+        {
+            // Arrange
+            SetupValidatorToBeValid();
+
+            // Act
+            await Instance.DeleteOne(_stringValue, _stringValue);
+
+            // Assert
+            GetMockFor<IClientDataRespository>().Verify(r => r.DeleteOne(_stringValue, _stringValue), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task DeleteOne_IdsAreInvalid_RepositoryIsNeverCalled()
+        {
+            // Arrange
+            SetupValidatorToThrowExpection();
+            // Act
+            try
+            {
+                await Instance.DeleteOne(_stringValue, _stringValue);
+            }
+            catch
+            {
+                // Assert
+                GetMockFor<IClientDataRespository>().Verify(r => r.DeleteOne(_stringValue, _stringValue), Times.Never());
+            }
+        }
+
+        #endregion DeleteOne Tests
+
+        #region DeleteElementAll Tests
+
+        [TestMethod]
+        public async Task DeleteElementAll_IdsAreValid_RepositoryIsCalled()
+        {
+            // Arrange
+            SetupValidatorToBeValid();
+
+            // Act
+            await Instance.DeleteOne(_stringValue, _stringValue);
+
+            // Assert
+            GetMockFor<IClientDataRespository>().Verify(r => r.DeleteOne(_stringValue, _stringValue), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task DeleteElementAll_IdsAreInvalid_RepositoryIsNeverCalled()
+        {
+            // Arrange
+            SetupValidatorToThrowExpection();
+            // Act
+            try
+            {
+                await Instance.DeleteElementAll(_stringValue, _stringValue);
+            }
+            catch
+            {
+                // Assert
+                GetMockFor<IClientDataRespository>().Verify(r => r.DeleteElementAll(_stringValue, _stringValue), Times.Never());
+            }
+        }
+
+        #endregion DeleteElementAll Tests
+
+        #region DeleteElementOne Tests
+
+        [TestMethod]
+        public async Task DeleteElementOne_IdsAreValid_RepositoryIsCalled()
+        {
+            // Arrange
+            SetupValidatorToBeValid();
+
+            // Act
+            await Instance.DeleteElementOne(_stringValue, _stringValue, _stringValue);
+
+            // Assert
+            GetMockFor<IClientDataRespository>().Verify(r => r.DeleteElementOne(_stringValue, _stringValue, _stringValue), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task DeleteElementOne_IdsAreInvalid_RepositoryIsNeverCalled()
+        {
+            // Arrange
+            SetupValidatorToThrowExpection();
+            // Act
+            try
+            {
+                await Instance.DeleteElementOne(_stringValue, _stringValue, _stringValue);
+            }
+            catch
+            {
+                // Assert
+                GetMockFor<IClientDataRespository>().Verify(r => r.DeleteElementOne(_stringValue, _stringValue, _stringValue), Times.Never());
+            }
+        }
+
+        #endregion DeleteElementOne Tests
+
+        private void SetupValidatorToBeValid()
+        {
+            GetMockFor<IValidator>().Setup(v => v.IsValidUserId(_stringValue)).Returns(true);
+        }
+
+        private void SetupValidatorToThrowExpection()
+        {
+            GetMockFor<IValidator>().Setup(v => v.IsValidUserId(It.IsAny<string>())).Throws(new ArgumentException("invalid user id"));
+            GetMockFor<IValidator>().Setup(v => v.IsValidDataStoreId(It.IsAny<string>())).Throws(new ArgumentException("invalid datastore id"));
+            GetMockFor<IValidator>().Setup(v => v.IsValidElementId(It.IsAny<string>())).Throws(new ArgumentException("invalid element id"));
+            GetMockFor<IValidator>().Setup(v => v.IsValidJson(It.IsAny<string>())).Throws(new ArgumentException("invalid json"));
+            GetMockFor<IValidator>().Setup(v => v.IsValidDataStoreName(It.IsAny<string>())).Throws(new ArgumentException("invalid datastore name"));
         }
 
         #endregion ClientDataManage Tests
