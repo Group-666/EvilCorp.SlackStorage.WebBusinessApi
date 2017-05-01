45aba1773b263233809fc345110fd260ad72a7fd
EliiseS <selingeliise@gmail.com>
2017-05-01 20:04:21 +0200

    Implemented Account Repository

--- src/WebApi.Data/AccountRepository.cs
+++ src/WebApi.Data/AccountRepository.cs
@@ -1,7 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using RestSharp;
+using System.Net;
+using System.Threading.Tasks;
 using System.Xml.Linq;
-using RestSharp;
 using WebApi.Domain.Contracts;
+using WebApi.Domain.Entities;
 
 namespace WebApi.Data
 {
@@ -11,7 +13,7 @@ namespace WebApi.Data
         private const string Repositoryurl = "http://localhost:53879/UserService.svc/";
 #else
 //Real URL
-        private const string REPOSITORYURL = "http://localhost/api/storage";
+        private const string Repositoryurl = "http://localhost:53879/UserService.svc/";
 #endif
         private readonly ILogger _logger;
         private readonly RestClient _restClient;
@@ -22,38 +24,69 @@ namespace WebApi.Data
             _logger = logger;
         }
 
-        public async Task<string> GetAll(string userId)
+        public async Task<string> Create(XDocument xml)
         {
-            var request = new RestRequest(userId, Method.GET);
+            var request = new RestRequest("registeruser", Method.POST) { RequestFormat = DataFormat.Xml };
+            request.AddParameter("text/xml", xml, ParameterType.RequestBody);
+
             var result = await _restClient.ExecuteTaskAsync(request);
 
-            return result.Content;
+            return LogOnErrorReturnContent(result);
         }
 
         public async Task<string> Login(string userId, string passwordHash)
         {
-            //var request = new RestRequest(userId + "/" + passwordHash, Method.GET);
-            //var result = await _restClient.ExecuteTaskAsync(request);
-
-            var requestss = new RestRequest("login/" + userId + "/" + passwordHash, Method.GET)
-            {
-                RequestFormat = DataFormat.Xml
-            };
+            var requestss = new RestRequest("login/" + userId + "/" + passwordHash, Method.GET) { RequestFormat = DataFormat.Xml };
             var response = await _restClient.ExecuteTaskAsync(requestss);
 
-            var login = new login();
-            login.userId = response.Content;
-            var xElement = XElement.Parse(response.Content);
-            var value = xElement.Value;
-
-            return response.Content;
+            return LogOnErrorReturnContent(response);
         }
 
-        private class login
+        public async Task<string> GetAll()
         {
-            public string username;
-            public string password;
-            public string userId;
+            var request = new RestRequest("getalluser", Method.GET) { RequestFormat = DataFormat.Xml };
+            var response = await _restClient.ExecuteTaskAsync(request);
+
+            return LogOnErrorReturnContent(response);
+        }
+
+        public async Task<string> GetOne(string userId)
+        {
+            var request = new RestRequest(userId, Method.GET) { RequestFormat = DataFormat.Xml };
+            var response = await _restClient.ExecuteTaskAsync(request);
+
+            return LogOnErrorReturnContent(response);
+        }
+
+        public async Task<string> Disable(string userId)
+        {
+            var request = new RestRequest("disableuser/" + userId, Method.GET) { RequestFormat = DataFormat.Xml };
+            var response = await _restClient.ExecuteTaskAsync(request);
+
+            return LogOnErrorReturnContent(response);
+        }
+
+        public async Task<string> Enable(string userId)
+        {
+            var request = new RestRequest("enableuser/" + userId, Method.GET) { RequestFormat = DataFormat.Xml };
+            var response = await _restClient.ExecuteTaskAsync(request);
+
+            return LogOnErrorReturnContent(response);
+        }
+
+        public async Task<string> Delete(string userId)
+        {
+            var request = new RestRequest("deleteuser/" + userId, Method.GET) { RequestFormat = DataFormat.Xml };
+            var response = await _restClient.ExecuteTaskAsync(request);
+
+            return LogOnErrorReturnContent(response);
+        }
+
+        private string LogOnErrorReturnContent(IRestResponse result)
+        {
+            if (result.StatusCode != HttpStatusCode.OK)
+                _logger.Log(string.IsNullOrEmpty(result.ErrorMessage) ? result.ToString() : result.ErrorMessage, LogLevel.Critical);
+            return result.Content;
         }
     }
 }
--- src/WebApi.Domain/Contracts/IAccountRepository.cs
+++ src/WebApi.Domain/Contracts/IAccountRepository.cs
@@ -1,9 +1,22 @@
 ﻿using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace WebApi.Domain.Contracts
 {
     public interface IAccountRepository
     {
+        Task<string> Create(XDocument xml);
+
         Task<string> Login(string userId, string passwordHash);
+
+        Task<string> GetAll();
+
+        Task<string> GetOne(string userId);
+
+        Task<string> Disable(string userId);
+
+        Task<string> Enable(string userId);
+
+        Task<string> Delete(string userId);
     }
 }
