ec3c8155b5f604e7ba54e034c8200ec348f50162
EliiseS <selingeliise@gmail.com>
2017-05-01 20:02:15 +0200

    Implemented converter

---
+++ src/WebApi.Business/Converter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using WebApi.Domain.Contracts;
+
+namespace WebApi.Business
+{
+    public class Converter : IConverter
+    {
+        public string ConvertXmlToString(string xml)
+        {
+            var doc = XDocument.Parse(xml);
+            return JsonConvert.SerializeXNode(doc, Formatting.None, true);
+        }
+
+        public XDocument ConvertCreateJsonToXml(JObject json)
+        {
+            var xmlIn = JsonConvert.DeserializeXNode(json.ToString(), "User");
+            return new XDocument(new XElement("RegisterUser", xmlIn.Root));
+        }
+    }
+}
